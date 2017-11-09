7fe1239d7f7fef2c34270b34470944d42dfa62d4
Martin Češnovar <martin.cesnovar@gmail.com>
2017-11-09 11:05:49 +0100

    dodajOsebo

--- banka.py
+++ banka.py
@@ -1,6 +1,5 @@
-#Testni projekt o banki
+# Testni projekt o banki.
 import sqlite3
-import curses
 
 baza = "banka1.db"
 
@@ -9,11 +8,13 @@ class BancniTerminal:
         self.oseba = None
         self.racun = None
         self.cur = None
+        self.con = None
         self.menu = "glavni"
         self.zazeni()
         
     def zazeni(self):
         with sqlite3.connect(baza) as con:
+            self.con = con
             self.cur = con.cursor()
             while True:
                 if self.menu == "glavni":
@@ -24,10 +25,12 @@ class BancniTerminal:
                     self.dodajOsebo()
                 elif self.menu == "izpisRacunov":
                     self.izpisRacunov()
+                elif self.menu == "oOsebi":
+                    self.oOsebi()
 
     def glavniMenu(self):
         print("-"*10)
-        print("O - Pregled Osebe")
+        print("O - Pregled Oseb")
         print("X - Izhod")
         izbira = input("> ")
         if izbira.lower() == "o":
@@ -57,16 +60,31 @@ class BancniTerminal:
             n = int(izbira) - 1
             if n >= 0 and n < len(osebe):
                 self.oseba = osebe[n]
-                self.menu = "izpisRacunov"
+                self.menu = "oOsebi"
             return
             
     def dodajOsebo(self):
-        print("Zdaj bi rad dodal osebo.")
+        print("Dodajanje nove osebe")
+        ime = input("Ime: ")
+        priimek = input("Priimek: ")
+        emso = input("EMŠO: ")
+        ulica = input("Ulica: ")
+        stevilka = input("Hišna številka: ")
+        posta = input("Poštna številka: ")
+        try:
+            self.cur.execute("INSERT INTO Oseba (IME, PRIIMEK, EMSO, ULICA, HISNA_STEVILKA, POSTA)\
+    values (?,?,?,?,?,?)", (ime, priimek, emso, ulica, stevilka, posta))
+            self.con.commit()
+        except Exception as e:
+            print("Neuspešen vnos. Poskusi ponovno.", e)
+        print("Vnos osebe", ime, priimek, "uspešen")     
         self.menu = "glavni"
 
+    def oOsebi(self):
+        print("Podatki o osebi")
+        
     def izpisRacunov(self):
         print("Izpis racunov za ", self.oseba)
         self.menu = "glavni"
         
 BancniTerminal()
-
